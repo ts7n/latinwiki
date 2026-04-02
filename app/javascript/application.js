6ed7b69f2ea12b5135656668bbf3aa2ad2e5638f
ts7n <65257768+ts7n@users.noreply.github.com>
2026-04-02 02:20:41 -0500

    setup posthog analytics

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import posthog from "posthog-js"
+
+posthog.init('phc_xiDq5jaDS7kEuFQAdVXc9i4WL6KvPBsNRhGNMMESZFgJ', {
+    api_host: 'https://phproxy.tlampert.net',
+    defaults: '2026-01-30'
+})
